553b1c5bb7a51d425689eba2179cd288c9a5352e
Guilherme Ariel Dias <diasarielguilherme@gmail.com>
2024-03-26 18:42:20 -0300

    Initial commit

---
+++ desafiosaula4.py
@@ -0,0 +1,11 @@
+nome = input('Qual é o seu nome?')
+print('Olá', nome,  '! Prazer em te Conhecer!')
+
+dia = input('Dia:')
+mes = input('Mês:')
+ano = input('Ano:')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano, '. Correto?')
+
+nm1 = input('Primeiro Número')
+nm2 = input('Segundo Número')
+print('A soma dos números é',int(nm1) + int(nm2))
